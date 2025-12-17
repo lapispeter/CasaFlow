3aa66f8adde76462ad3cfba16811bac4e19c7c67
Lapis Péter <lapispeter69@gmail.com>
2025-12-17 20:16:29 +0100

    folytatás

--- src/app/pages/modules/bill/bill.ts
+++ src/app/pages/modules/bill/bill.ts
@@ -1,25 +1,41 @@
 import { Component } from '@angular/core';
 import { BillService } from '../../../services/bill-service';
 import { DatePipe } from '@angular/common';
+import { Form, FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-bill',
-  imports: [DatePipe],
+  imports: [DatePipe, ReactiveFormsModule],
   templateUrl: './bill.html',
   styleUrl: './bill.css',
 })
 export class Bill {
 
-  bills: any;
+  bills!: any;
+  billForm: any;
 
   showModal=false;
-  addmode=true;
+  addMode=true;
 
-  constructor(private api: BillService) { }
+  constructor(private api: BillService,
+    private builder: FormBuilder
+  ) { }
 
   ngOnInit() {
     this.getBills();
+    this.initForm();
+  }
+
+  initForm () {
+    this.billForm = this.builder.group({
+      billType: [''],
+      amount: [''],
+      date: [''],
+      paymentStatus: ['']
+    })
+
+
   }
 
   getBills() {
@@ -33,20 +49,43 @@ export class Bill {
       },
     });
   }
-  createBill () {
+  startShowModal () {
     this.showModal=true;
   } 
 
-  cancelBill () {
+  cancel () {
     this.showModal=false;
   }
 
-  saveBill () {
+  startSave() {
+    console.log('save');
     this.showModal=false;
+    if(this.addMode) {
+      this.startAddBill();
+    } else {
+      this.startUpdateBill();
+    
+    }
   }
-
-  updateBill () {}
-  deleteBill () {} 
+  startAddBill () {
+    console.log(this.billForm.value);
+    const newBill= {
+      billType: this.billForm.value.billType,
+      amount: this.billForm.value.amount,
+      date: this.billForm.value.date,
+      paymentStatus: this.billForm.value.paymentStatus
+    }
+    this.api.createBill(newBill).subscribe({
+      next: (res: any) => {
+        console.log(res)
+        this.getBills();
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+  startUpdateBill () {} 
 
 
 }
